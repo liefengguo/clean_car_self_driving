2e378119c2f6392d92348e54835fd13c9244bd31
liefengguo <1321755404@qq.com>
2023-09-04 09:34:38 +0800

    add udp_action_app

---
+++ src/udp_action_app.cpp
@@ -0,0 +1,38 @@
+#include "udp_action.h"
+#include <ros/ros.h>
+#include <std_msgs/Bool.h>
+
+class BoolSubscriber {
+public:
+    BoolSubscriber() {
+        // 初始化ROS节点和订阅者
+        ros::NodeHandle nh;
+        udp_action robot_action;
+        bool_subscriber = nh.subscribe("pump_app", 10, &BoolSubscriber::boolCallback, this);
+    }
+
+    void boolCallback(const std_msgs::Bool::ConstPtr& msg) {
+        // 在这里处理接收到的消息，可以调用类的其他方法或执行其他操作
+        if (msg->data) {
+            robot_action.pump(1);
+            ROS_INFO("Received true");
+        } else {
+            ROS_INFO("Received false");
+            robot_action.pump(0);
+        }
+    }
+
+private:
+    ros::Subscriber bool_subscriber;
+};
+
+int main(int argc, char** argv) {
+    ros::init(argc, argv, "bool_subscriber_node");
+
+    BoolSubscriber bool_subscriber;
+
+    // 进入ROS循环
+    ros::spin();
+
+    return 0;
+}
